3f6d832cc59682384fc46686dfd2320b6899c981
Nicklin095 <nicklin095@gmail.com>
2017-04-12 18:29:32 +0800

    Add files via upload

--- NNresult/network_update.m
+++ NNresult/network_update.m
@@ -4,35 +4,26 @@
 
 function [brnn]=network_update(brnn, input_data)
 
-% 決定誤差
-% activation.error=input_data-brnn.Y;
-
 % 更新w3矩陣
 w3_temp=brnn.w3;
-% w2_temp=brnn.w2;
 w1_temp=brnn.w1;
 
-% activation.delta_s=activation.error;
+% 決定誤差
 activation.delta_s=(input_data-brnn.Y)*(brnn.Y*brnn.Y*(1-brnn.Y));
+
 for j=1:brnn.number_of_output
     for i=1:brnn.number_of_neural1
         brnn.w3(j,i)=brnn.w3(j,i)+brnn.eta_wbb*activation.delta_s(j,1)*brnn.oq(i,1);
     end
 end
-temp = (1-brnn.fq)*brnn.fq'
-activation.delta_q=(activation.delta_s*brnn.w1)*(brnn.op*temp);
-% 更新w1 and d1 矩陣
+% 更新w1矩陣
 for j=1:brnn.number_of_output
     for i=1:brnn.number_of_neural1
-        activation.delta_q(i,1)=activation.delta_q(i,1)+activation.delta_s(j,1)*w3_temp(j,i)*4/(brnn.pos_exp+ brnn.minu_exp)^2;
+        activation.delta_q(i,1)=w3_temp(j,i)*activation.delta_s(j,1)*(brnn.w1(j,i)*(1-brnn.w1(j,i)))
     end
 end
-brnn.d1=brnn.d1+brnn.eta_wbb*activation.delta_q;
-for j=1:brnn.number_of_neural1
-    for i=1:brnn.number_of_input
-        brnn.w1(j,i)=brnn.w1(j,i)+brnn.eta_wbb*activation.delta_q(j,1)*brnn.op(i,1);
-    end
-end
+brnn.w1=w3_temp+brnn.eta_wbb*activation.delta_q;
+
 
 
 
